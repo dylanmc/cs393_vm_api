03f6bebf018c6bb7fed0c794d32838b42083320b
Dylan McNamee <dylan.mcnamee@gmail.com>
2023-02-19 18:17:57 -0800

    ideas / thoughts for data structures for storing mappings

--- src/address_space.rs
+++ src/address_space.rs
@@ -12,8 +12,18 @@ struct MapEntry {
 }
 pub struct AddressSpace {
     name: String,
-    mappings: LinkedList<MapEntry>,
+    mappings: LinkedList<MapEntry>,  // see below for comments
 }
+
+// comments about storing mappings
+// Most OS code uses doubly-linked lists to store sparse data structures like
+// an address space's mappings.
+// Using Rust's built-in LinkedLists is fine. See https://doc.rust-lang.org/std/collections/struct.LinkedList.html
+// But if you really want to get the zen of Rust, this is a really good read, written by the original author
+// of that very data structure: https://rust-unofficial.github.io/too-many-lists/
+
+// So, feel free to come up with a different structure, either a classic Rust collection,
+// from a crate (but remember it needs to be #no_std compatible), or even write your own.
 
 impl AddressSpace {
     pub fn new(name: &str) -> Self {
